343b3fb8b0c0c2641a3750f1c5f8f4448ba4467e
Nuwan <nuwan199044@gmail.com>
2022-12-20 13:33:21 +0530

    Added CSV data to in memory database

--- src/main/java/io/itgen/ipldashboardservice/data/JobCompletionNotificationListener.java
+++ src/main/java/io/itgen/ipldashboardservice/data/JobCompletionNotificationListener.java
@@ -1,6 +1,9 @@
 package io.itgen.ipldashboardservice.data;
 
 import io.itgen.ipldashboardservice.model.Match;
+import io.itgen.ipldashboardservice.model.Team;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
@@ -10,29 +13,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-    private final JdbcTemplate jdbcTemplate;
+    private final EntityManager entityManager;
 
     @Autowired
-    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public JobCompletionNotificationListener(EntityManager entityManager) {
+        this.entityManager = entityManager;
     }
 
     @Override
+    @Transactional
     public void afterJob(JobExecution jobExecution) {
         if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("!!! JOB FINISHED! Time to verify the results");
 
-            jdbcTemplate.query("SELECT id, team1, team2 FROM match",
-                    (rs, row) ->
-                            rs.getLong(1) + " " +
-                            rs.getString(2) + " " +
-                            rs.getString(3)
-            ).forEach(str -> System.out.println(str));
+            Map<String, Team> teamData = new HashMap<>();
+
+            entityManager.createQuery("SELECT m.team1, count(*) from Match m group by m.team1", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .map(e -> new Team((String)e[0], (long)e[1]))
+                    .forEach(team -> teamData.put(team.getTeamName(), team));
+
+            entityManager.createQuery("SELECT m.team2, count(*) from Match m group by m.team2", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .map(e -> {
+                        Team t = teamData.get((String) e[0]);
+                        if (t != null) {
+                            t.setTotalMatches(t.getTotalMatches() + (long) e[1]);
+                        }
+                        return t;
+                    })
+                    .forEach(t -> teamData.put(t.getTeamName(), t));
+
+            entityManager.createQuery("SELECT m.winner, count(*) from Match m group by m.winner", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .forEach(e -> {
+                        Team t = teamData.get((String) e[0]);
+                        if (t != null) {
+                            t.setTotalWins((long) e[1]);
+                        }
+                    });
+
+            teamData.values().forEach(team -> entityManager.persist(team));
+            teamData.values().forEach(team -> System.out.println(team));
         }
     }
 }
---
+++ src/main/java/io/itgen/ipldashboardservice/model/Team.java
@@ -0,0 +1,35 @@
+package io.itgen.ipldashboardservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    public Team(String teamName, long totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "teamName='" + teamName + '\'' +
+                ", totalMatches=" + totalMatches +
+                ", totalWins=" + totalWins +
+                '}';
+    }
+}
